10be8ea3c3311598a863c7ae85e5eb5968528f12
Nimasha Gamage <nimasha.piyumini@gmail.com>
2024-03-16 23:38:09 +0530

    Remove server directory in favour of backend

--- server/routes/filter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const filterController = require('../controllers/filter');
-
-
-router.get('/', filterController.getNewArrivals);
-router.post('/search', filterController.searchByQueryType);
-
-module.exports = router;
