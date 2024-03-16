3de09960f185c783d2976b8dcae06b947bc18641
Nimasha Gamage <nimasha.piyumini@gmail.com>
2024-03-16 23:37:45 +0530

    Remove client directory in favour of frontend

--- client/src/api/product.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-
-export const createProduct = async(data) => {
-    const response = await axios.post('http://localhost:5002/api/product',data);
-
-    return response;
-
-};
