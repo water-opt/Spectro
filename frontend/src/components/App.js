0843bde1d2b33a57a811fe0b37b38024b4195f66
Nimasha Gamage <nimasha.piyumini@gmail.com>
2024-03-19 01:32:33 +0530

    Comment out riders and vehicles dashboards on App.js because of conflicts with product management admin dashboard

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -12,10 +12,10 @@ import Cart from '../pages/CartPage';
 import Orders from '../pages/OrdersPage';
 import OrderDet from '../pages/OrderDetails';
 import Rider from '../pages/RiderDetails'
-import RiderDash from '../pages/RidersDash';
+// import RiderDash from '../pages/RidersDash'; // TODO: please fix errors here. This component conflicts with Product Management Admin Dashboard.
 import RiderDet from '../pages/RiderDet';
 import Vehicle from '../pages/VehicleDetails'
-import VehicleDash from '../pages/VehiclesDash';
+// import VehicleDash from '../pages/VehiclesDash'; // TODO: please fix errors here. This component conflicts with Product Management Admin Dashboard.
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 const App = () => {
@@ -46,9 +46,9 @@ const App = () => {
 
                     {/* Routes for vehicles & riders */}
                     <Route path='/vehicles/registrationform' element={<Vehicle/>}/>
-                    <Route path='/vehicles/dashboard' element={<VehicleDash/>}/>
+                    {/* <Route path='/vehicles/dashboard' element={<VehicleDash/>}/> */}  {/* TODO: please fix errors here. This component conflicts with Product Management Admin Dashboard.*/}
                     <Route path='/riders/registrationform' element={<Rider/>}/>
-                    <Route path='/riders/dashboard' element={<RiderDash/>}/>
+                    {/* <Route path='/riders/dashboard' element={<RiderDash/>}/> */} {/* TODO: please fix errors here. This component conflicts with Product Management Admin Dashboard.*/}
                     <Route path='/riders/:id' element={<RiderDet/>}/>
 
                 </Routes>
